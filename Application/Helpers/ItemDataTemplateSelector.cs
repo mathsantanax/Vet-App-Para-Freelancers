5033ebdba460bc44963344ad1b1421158e6710cb
Matheus Santana <matheus.santana7@hotmail.com>
2025-04-08 15:59:48 -0300

    alterando csproj e solution

--- Application/Helpers/ItemDataTemplateSelector.cs
+++ Application/Helpers/ItemDataTemplateSelector.cs
@@ -9,20 +9,20 @@ namespace Vet_App_For_Freelancers.Helpers
 {
     public class ItemDataTemplateSelector : DataTemplateSelector
     {
-        public DataTemplate ServicoTemplate { get; set; }
-        public DataTemplate ProdutoTemplate { get; set; }
+        public DataTemplate? ServicoTemplate { get; set; }
+        public DataTemplate? ProdutoTemplate { get; set; }
 
         protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
         {
             if (item is ItemServico)
             {
-                return ServicoTemplate;
+                return ServicoTemplate!;
             }
             else if (item is ItemAtendimento)
             {
-                return ProdutoTemplate;
+                return ProdutoTemplate!;
             }
-            return null;
+            return null!;
         }
     }
 }
